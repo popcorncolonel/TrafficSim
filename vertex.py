54468b423e2951cac1884f9b41369844e4ff93ed
Graham Goudeau <grahamgoudeau@gmail.com>
2015-11-04 15:41:35 -0500

    Changed vertex to keep track of incoming and outgoing

--- vertex.py
+++ vertex.py
@@ -1,7 +1,11 @@
 class Vertex(object):
-    def __init__(self, edge_set=[]):
-        self.edge_set = edge_set
+    def __init__(self, incoming_edge_set=[], outgoing_edge_set=[]):
+        self.incoming_edge_set = set(incoming_edge_set)
+        self.outgoing_edge_set = set(outgoing_edge_set)
 
     # add an edge with this vertex as the starting point
-    def add_edge(self, edge):
-        self.edge_set.append(edge)
+    def add_outgoing_edge(self, edge):
+        self.outgoing_edge_set.add(edge)
+
+    def add_incoming_edge(self, edge):
+        self.incoming_edge_set.add(edge)
