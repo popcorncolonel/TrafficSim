1a7803acad9fd6b110953e73128dba82897b6fcb
Eric Bailey <popcorncolonel@gmail.com>
2015-11-04 16:15:29 -0500

    added some more car logic

--- car.py
+++ car.py
@@ -9,22 +9,29 @@ class Car(object):
         self.road = road # Road object.
         self.road_position = init_road_progress # In feet.
         self.destination = destination # Destination object.
-        # TODO: calculate list of roads to go on to get to the
-        #       destination (dijkstra)
+        # TODO: Calculate list of roads to go on to get to the
+        #       destination (dijkstra).
 
-        # TODO: spawn internal thread which calls update_status in a loop
+        # TODO: Spawn internal thread which calls update_status in a loop.
     
+
     def update_status(self, time_since_last_update=0.1):
         # Update position (based on velocity)
-        position += velocity * time_since_last_update
+        self.road_position += velocity * time_since_last_update
         if self.road_position == self.road.length:
-            print 'uh oh'
-
+            print 'uh oh' 
+ 
         # Update velocity (based on acceleration)
-        velocity += acceleration * time_since_last_update
-        pass
+        self.velocity += self.acceleration * time_since_last_update
 
-    def update_acceleration(self, acceleration):
-        # Initially instantaneous acceleration.
+        # Slow down before intersections.
+        dist_to_finish = self.road.length - self.road_position
+        if dist_to_finish < 100.0: # TODO: change this to be a reasonable percent
+            pass
+            # start slowing down
+
+
+    # Initially instantaneous acceleration.
+    def set_acceleration(self, acceleration):
         self.acceleration = acceleration
 
