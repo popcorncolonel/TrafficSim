70adff66973a39ac4d20914f082852113b87940f
Eric Bailey <popcorncolonel@gmail.com>
2015-11-04 15:42:16 -0500

    correct inheritence

--- intersection.py
+++ intersection.py
@@ -1,13 +1,12 @@
-#from vertex import Vertex
+from vertex import Vertex
 
 # Intersections are the base Nodes to convey location
-class Intersection(object):
+class Intersection(Vertex):
     '''
     x and y are in Feet
     '''
     def __init__(self, x, y, outgoing_roads=[], incoming_roads=[]):
+        Vertex.__init__(incoming_roads, outgoing_roads)
         self.x = x
         self.y = y
-        self.outgoing_roads = outgoing_roads
-        self.incoming_roads = incoming_roads
 
--- road.py
+++ road.py
@@ -1,17 +1,15 @@
+from edge import Edge
 from car import Car
 from math import sqrt
 
-class Road(object):
+class Road(Edge):
     '''
-    Length is a float, representing the length (in feet) of the road).
-    Start is an Intersection.
-    End is an Intersection.
+    start is an Intersection.
+    end is an Intersection.
     '''
     def __init__(self, start, end):
+        Edge.__init__(start, end)
         self.cars = []
-
-        self.start = start
-        self.end = end
 
         self.length = math.sqrt((end.y-start.y)^2 + (end.x-start.x)^2)
 
