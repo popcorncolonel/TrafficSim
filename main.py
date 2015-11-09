5ebbd06088ed6b983ae63e59715ade43b37af9df
Eric Bailey <popcorncolonel@gmail.com>
2015-11-09 15:30:07 -0500

    main works!!!

--- main.py
+++ main.py
@@ -2,8 +2,20 @@ import sys
 from edge import Edge
 from intersection import Intersection
 from road import Road
+from source import Source, Destination
+from graphics import *
+import time
 
 def main():
+    w = Window()
+    s = Sprite('red-car.png')
+    w.addSprite(s)
+    w.refresh()
+    x = y = 0
+    while True:
+        s.move(x=5)
+        w.refresh()
+        time.sleep(0.01)
     print ':)'
 
 if __name__ == '__main__':
--- source.py
+++ source.py
@@ -2,23 +2,33 @@ from car import Car
 from vertex import Vertex
 
 class Source(Vertex):
-    def __init__(self, road, length_along_road):
+    def __init__(self, road, length_along_road, cars=[], generative=False):
         Vertex.__init__(self, incoming_edge_set=[road])
         self.road = road
         self.length_along_road = length_along_road
+        self.cars = set(cars) # Cars waiting to go
+        self.generative = generative
     
     def spawn_car(self):
-        new_car = Car(self.road, init_road_progress = self.length_along_road)
-        self.road.add_car(new_car)
+        if self.generative:
+            new_car = Car(self.road, init_road_progress = self.length_along_road)
+            self.road.add_car(new_car)
+        else:
+            # remove a car from the set
+            pass
 
 
 class Destination(Vertex):
-    def __init__(self, road, length_along_road):
+    def __init__(self, road, length_along_road, destructive=False):
         Vertex.__init__(self, outgoing_edge_set=[road])
         self.road = road
         self.length_along_road = length_along_road
+        self.destructive = destructive
+        self.cars = set()
     
-    def remove_car(self):
-        self.road.remove_car(new_car)
+    def remove_car(self, car):
+        self.road.remove_car(car)
+        if not self.destructive:
+            self.cars.add(car)
         
 
