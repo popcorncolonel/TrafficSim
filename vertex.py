0f649b838943b237c8c6aa58e83d141d07d5aee7
Eric Bailey <popcorncolonel@gmail.com>
2015-12-11 12:31:14 -0500

    solid docs

--- vertex.py
+++ vertex.py
@@ -1,4 +1,5 @@
 class Vertex(object):
+    ''' Abstract class that things just inherit from. '''
     def __init__(self, incoming_edge_set=[], outgoing_edge_set=[]):
         self.incoming_edge_set = set(incoming_edge_set)
         self.outgoing_edge_set = set(outgoing_edge_set)
